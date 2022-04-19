00fb23ed3d9ffa562885011055af2fc8430c2e28
Oscar Aguilar <aguilop.oscar@gmail.com>
2022-04-19 00:23:41 -0500

    Se anadio soporte para reportes en formato .xlsx

--- academico/admin.py
+++ academico/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
 from academico.models import Curso
 
 # Register your models here.
 
 
-admin.site.register(Curso)
+class CursoResources(resources.ModelResource):
+    fields = {
+        'codigo',
+        'nombre',
+        'creditos',
+    }
+
+    class Meta:
+        model = Curso
+
+
+@admin.register(Curso)
+class CursoAdmin(ImportExportModelAdmin):
+    resource_class = CursoResources
--- universidad/settings.py
+++ universidad/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'academico',
+    'import_export',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -117,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
